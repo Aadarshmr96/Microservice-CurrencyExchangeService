8260bd8b1ce04a66689e98593757bcb43a336e13
rohan <iamrohanjainn@gmail.com>
2021-04-23 16:42:25 +0530

    add eureka server

---
+++ naming-server/src/test/java/com/mircroservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mircroservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
